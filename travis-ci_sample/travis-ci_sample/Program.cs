d8c102d3f7e29c4e542dcd3a85b2dfcd36237abb
Кормилицын Андрей Владиславович <andreyka.one.two@gmail.com>
2018-04-16 20:33:19 +0300

    Initial commit with travis test config

---
+++ travis-ci_sample/travis-ci_sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace travis_ci_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
